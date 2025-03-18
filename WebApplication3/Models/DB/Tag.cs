a26df7a505926a61646831f3c89a7717f81cf034
edenp-org <admin@edenp.org>
2025-03-18 22:08:34 +0800

    Create Tag.cs
    feat：增加tag数据库类

---
+++ WebApplication3/Models/DB/Tag.cs
@@ -0,0 +1,31 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace WebApplication3.Models.DB
+{
+    /// <summary>
+    /// 标签类，表示数据库中的标签表
+    /// </summary>
+    public class Tag
+    {
+        /// <summary>
+        /// 标签ID，主键，自增
+        /// </summary>
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 标签名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 标签描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
